068e66a6fcec083f4dc51b76aedb3dda088781ce
gdgrant <gdgrant@uchicago.edu>
2018-08-23 15:47:01 -0500

    Placeholder

--- run_analysis.py
+++ run_analysis.py
@@ -4,10 +4,10 @@ from analysis import *
 
 task = "chunking"
 
-path = './savedir/one_hot_perfect/'
-file_list = ['sequence_cue_4_all_RF.pkl']#os.listdir(path)
+path = './savedir/' #one_hot_perfect/'
+file_list = ['sequence_cue_4_all_RF_100_neuron.pkl']#os.listdir(path)
 
 for f in file_list:
     print('Analyzing network...')
-    save_fn = './savedir/one_hot_perfect/analysis_' + f
+    save_fn = './savedir/analysis_' + f
     analyze_model_from_file(path + f, savefile=save_fn)
