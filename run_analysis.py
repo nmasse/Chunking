1686b4dc7ced4ae4f238916ae6de236e27f4a042
gdgrant <gdgrant@uchicago.edu>
2018-08-24 14:37:08 -0500

    analysis

--- run_analysis.py
+++ run_analysis.py
@@ -4,10 +4,15 @@ from analysis import *
 
 task = "chunking"
 
-path = './savedir/' #one_hot_perfect/'
-file_list = ['sequence_cue_4_all_RF_100_neuron.pkl']#os.listdir(path)
+path = './savedir/one_hot_perfect/'
+file_list = ['sequence_sequence_cue_4_all_RF.pkl']
+file_list = ['RF_cue_sequence_cue_sequence_4_all_RF_long_delacc90_ay.pkl']
 
 for f in file_list:
     print('Analyzing network...')
     save_fn = './savedir/analysis_' + f
-    analyze_model_from_file(path + f, savefile=save_fn)
+
+    try:
+        analyze_model_from_file(path + f, savefile=save_fn)
+    except KeyboardInterrupt:
+        quit('Quit by KeyboardInterrupt')
