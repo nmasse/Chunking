0d2def1a3995767d08b5dd1ffdcc2c13e7e6631f
Catherine_Sylvia <xinyuw@uchicago.edu>
2018-09-05 12:35:52 -0500

    Small changes to run_ setups

--- run_analysis.py
+++ run_analysis.py
@@ -3,8 +3,19 @@ import sys, os
 import time
 from analysis import *
 
-task = "chunking"
+savedir = './savedir/'
+file = 'RF_cue_sequence_cue_p4_100_neuron_high_lr_v0_acc80.pkl'
 
+print('Analyzing network {}...'.format(file))
+save_fn = savedir + 'new/analysis_' + file
+
+try:
+    analyze_model_from_file(savedir+file, savefile=save_fn)
+except KeyboardInterrupt:
+    quit('Quit by KeyboardInterrupt')
+
+
+"""
 path = './savedir/'
 file_list = os.listdir("./savedir/") #['RF_cue_sequence_cue_sequence_4_all_RF_long_delacc90_ay.pkl']
 analyzed = []
@@ -22,3 +33,4 @@ for i in range(1):
             except KeyboardInterrupt:
                 quit('Quit by KeyboardInterrupt')
     #time.sleep(1800)
+"""
